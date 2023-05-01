945ab94f480dba442311bc538f77dfa415b3560e
MarkHenyecz <business@henyeczmark.hu>
2023-05-01 22:56:37 +0100

    file modifications

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,13 +3,12 @@ fx_version 'adamant'
 game 'gta5'
 
 client_scripts {
-	'NativeUI.lua',
+	'Client/utils/NativeUI.lua',
 	'Config.lua',
 	'Client/*.lua'
 }
 
 server_scripts {
 	'Config.lua',
-	'@mysql-async/lib/MySQL.lua',
 	'Server/*.lua'
 }
